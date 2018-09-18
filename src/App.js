b0829626e98bd0c911b734fa28e2ad0c54b40ebe
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2018-09-18 12:06:28 -0300

    Start Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Head from "./Components/Head Components/Head";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Head />
+      </div>
+    );
+  }
+}
+
+export default App;
